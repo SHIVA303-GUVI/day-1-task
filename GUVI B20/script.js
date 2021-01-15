8c7b72db0c0ca14426edac28991418e987d878c9
SHIVA <75667319+SHIVA303-GUVI@users.noreply.github.com>
2021-01-15 13:33:58 +0530

    Delete script.js

--- GUVI B20/script.js
+++
@@ -1,9 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET','https://restcountries.eu/rest/v2/all',true)
-request.send()
-request.onload = function (){
-    var countrydata = JSON.parse(this.response)
-    console.log(countrydata)
- let total = countrydata.reduce((acc,ele)=>acc+ele.population, 0)
-console.log(total)
-}
